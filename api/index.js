2a7e6e2e8ec0f8fcba9df280b68a2244211f93e0
mchehe <2596844541@qq.com>
2020-07-22 20:05:43 +0800

    1.3

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-app.use('/api', createProxyMiddleware({ target: 'https://api.okzy.tv/api.php/provide/vod/at/json/', changeOrigin: true }));
-app.listen(8081);
