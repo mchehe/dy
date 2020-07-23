e0e49d257745d91c889a57ba3a6c0b2a2bee3042
mchehe <2596844541@qq.com>
2020-07-23 16:30:48 +0800

    c

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-    configureWebpack:{
-		externals: {
-			vue: 'Vue',
-			'vue-router': 'VueRouter',
-			axios: 'axios',
-			vant: 'vant',
-			clipboard: 'Clipboard',
-			dplayer: 'DPlayer',
-			'hls.js': 'Hls',
-			'vue-cookies': '$cookies'
-		},
-	},
-
-    publicPath: ''
-}
+module.exports = {}
